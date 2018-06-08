f82f6db2757dcd84c970b5c2a37f980fdf8bdd27
ibulgakov <ibulgakov@zuzex.com>
2018-06-08 12:21:50 +0300

    sum fix bottomnavview

--- app/src/main/java/com/example/ibulgakov/testapp/BottomNavActivity.java
+++ app/src/main/java/com/example/ibulgakov/testapp/BottomNavActivity.java
@@ -2,11 +2,17 @@ package com.example.ibulgakov.testapp;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.internal.BottomNavigationMenuView;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.ibulgakov.testapp.fragments.DashboardFragment;
@@ -21,6 +27,10 @@ public class BottomNavActivity extends AppCompatActivity {
     private String homeTag = "home_nav_tag";
     private String dashboardTag = "home_dashboard_tag";
     private String notificationTag = "home_notify_tag";
+
+    private TextView tvHomeCount;
+    private TextView tvDashboardCount;
+    private TextView tvNotifyCount;
 
     HashMap<String,Fragment> fragments = new HashMap<>();
 
@@ -75,7 +85,12 @@ public class BottomNavActivity extends AppCompatActivity {
         loadFragment(homeFragment);
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        BottomNavigationViewHelper.removeShiftMode(navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        addCustomViewToNavigation(navigation,R.id.navigation_home);
+        addCustomViewToNavigation(navigation,R.id.navigation_dashboard);
+        addCustomViewToNavigation(navigation,R.id.navigation_notifications);
     }
 
     private boolean loadFragment(Fragment fragment) {
@@ -88,6 +103,29 @@ public class BottomNavActivity extends AppCompatActivity {
             return true;
         }
         return false;
+    }
+
+    private void addCustomViewToNavigation(BottomNavigationView navigation, int navItemViewId){
+        BottomNavigationMenuView bottomNavigationMenuView = (BottomNavigationMenuView) navigation.getChildAt(0);
+
+        BottomNavigationItemView itemView = bottomNavigationMenuView.findViewById(navItemViewId);
+
+        View customView = LayoutInflater.from(this).inflate(R.layout.custom_nav_item, bottomNavigationMenuView, false);
+        switch (navItemViewId){
+            case R.id.navigation_home:
+                tvHomeCount = customView.findViewById(R.id.tv_tab_count);
+                tvHomeCount.setText("99+");
+                break;
+            case R.id.navigation_dashboard:
+                tvDashboardCount = customView.findViewById(R.id.tv_tab_count);
+                tvDashboardCount.setText("99+");
+                break;
+            case R.id.navigation_notifications:
+                tvNotifyCount = customView.findViewById(R.id.tv_tab_count);
+                tvNotifyCount.setText("99+");
+                break;
+        }
+        itemView.addView(customView);
     }
 
 }
